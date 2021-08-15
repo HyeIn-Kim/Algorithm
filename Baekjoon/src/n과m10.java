8f789085c509d75a1519451a427a8640fd288b09
HyeIn-Kim <fallingstars2749@gmail.com>
2021-08-15 11:39:41 +0900

    210814 백준 n과m10

---
+++ Baekjoon/src/n과m10.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class n과m10 {
+
+	static int N, M;
+	static int[] input;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		input = new int[N];
+		numbers = new int[M];
+		
+		for(int i = 0; i < N; i++) {
+			input[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(input);
+		Combination(0, 0);
+		System.out.println(sb);
+
+	}
+	
+	private static void Combination(int start, int cnt) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(numbers[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		HashSet<Integer> set = new HashSet<Integer>();
+		for(int i = start; i < N; i++) {
+			if(set.contains(input[i])) continue;
+			
+			set.add(input[i]);
+			numbers[cnt] = input[i];
+			Combination(i + 1, cnt + 1);
+		}
+	}
+
+}
