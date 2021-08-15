489c299e2abcbc4b0ef57adae0ed52b7670e1e76
HyeIn-Kim <fallingstars2749@gmail.com>
2021-08-15 11:11:25 +0900

    210814 백준 n과m2

---
+++ Baekjoon/src/n과m2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// 조합
+public class n과m2 {
+
+	static int N, M;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		numbers = new int[M];
+		
+		Combination(0, 0);
+		System.out.println(sb);
+	}
+	
+	private static void Combination(int start, int cnt) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(numbers[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = start; i < N; i++) {
+			numbers[cnt] = i + 1;
+			Combination(i + 1, cnt + 1);
+		}
+	}
+
+}
