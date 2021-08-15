f95925f053dac346102e0089ce25d6a95d075888
HyeIn-Kim <fallingstars2749@gmail.com>
2021-08-15 11:39:13 +0900

    210814 백준 n과m6

---
+++ Baekjoon/src/n과m6.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class n과m6 {
+
+	static int N, M;
+	static int[] input;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder(); 
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		input = new int[N];
+		numbers = new int[M];
+		
+		for(int i = 0; i < N; i++) {
+			input[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(input);
+		Combination(0, 0);
+		System.out.println(sb);
+	}
+	
+	private static void Combination(int start, int cnt) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(numbers[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = start; i < N; i++) {
+			numbers[cnt] = input[i];
+			Combination(i + 1, cnt + 1);
+		}
+	}
+
+}
