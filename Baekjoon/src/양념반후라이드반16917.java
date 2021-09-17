57e378dd341449f535555cc5e9da232ab5705ec0
김혜인 <fallingstars2749@gmail.com>
2021-09-18 00:14:46 +0900

    210918 백준 양념반후라이드반16917

---
+++ Baekjoon/src/양념반후라이드반16917.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 양념반후라이드반16917 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+		
+		int price = A * X + B * Y;
+		int max = Math.max(X, Y);
+		for(int i = 1; i <= max; i++) {
+			int price2 = (i * (C * 2)) + (((X-i < 0) ? 0 : X-i) * A) + (((Y-i < 0) ? 0 : Y-i) * B);
+			if(price2 < price) price = price2;
+		}
+		System.out.println(price);
+	}
+
+}
