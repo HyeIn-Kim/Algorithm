27639d7ad5a75c34bb256ca6537bdb31292aa3c2
김혜인 <fallingstars2749@gmail.com>
2021-10-20 11:27:52 +0900

    211020 아기상어16236 (PQ버전)

--- Baekjoon/src/아기상어16236.java
+++ Baekjoon/src/아기상어16236.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 아기상어16236 {
@@ -36,7 +34,6 @@ public class 아기상어16236 {
 	static int[][] map;
 	static int[] dr = {0, -1, 0, 1};
 	static int[] dc = {-1, 0, 1, 0};
-	static PriorityQueue<Node> fish;
 	static int size, cnt;
 	static int result;
 	
@@ -65,12 +62,12 @@ public class 아기상어16236 {
 	}
 
 	private static void babyShark(int r, int c) {
+		map[r][c] = 0;					// 상어 위치를 0으로
+		
 		while(true) {
-			getFish(r, c);					// BFS를 돌며 먹을 물고기들을 찾는다
-			if(fish.size() == 0) return;	// 먹을 물고기를 찾지 못했다면 종료
+			Node n = getFish(r, c);					// BFS를 돌며 먹을 물고기들을 찾는다
+			if(n == null) return;					// 찾지 못했다면 종료
 			
-			Node n = fish.poll();			// PQ에서 가장 가까운 물고기를 꺼내온다
-			map[r][c] = 0;					// 상어 위치를 0으로
 			map[n.r][n.c] = 0;				// 물고기를 먹었으니 빈칸으로
 			result += n.cnt;				// 결과에 상어가 이동한 시간만큼 더해주고
 			cnt++;							// 먹은 물고기 수 갱신
@@ -83,17 +80,18 @@ public class 아기상어16236 {
 		}
 	}
 
-	// 어차피 가장 가까운 물고기 하나만 구하면 되니까
-	// BFS를 따로 Queue 만드는 게 아니라 PQ로 짰어도 됐었을 것 같다!!
-	private static void getFish(int r, int c) {
-		fish = new PriorityQueue<>();
+	private static Node getFish(int r, int c) {
+		PriorityQueue<Node> fish = new PriorityQueue<>();
 		boolean[][] visited = new boolean[N][N];
-		Queue<Node> queue = new LinkedList<>();
-		queue.offer(new Node(r, c, 0));
+		fish.offer(new Node(r, c, 0));
 		visited[r][c] = true;
 		
-		while(!queue.isEmpty()) {
-			Node n = queue.poll();
+		while(!fish.isEmpty()) {
+			Node n = fish.poll();
+			
+			if(0 < map[n.r][n.c] && map[n.r][n.c] < size) {
+				return n;
+			}
 			
 			for(int d = 0; d < 4; d++) {
 				int nr = n.r + dr[d];
@@ -104,12 +102,11 @@ public class 아기상어16236 {
 				if(visited[nr][nc]) continue;
 				// 크기가 크면 못지나가므로 pass
 				if(map[nr][nc] > size) continue;
-				// 먹을 수 있는 물고기는 PQ에 삽입
-				if(0 < map[nr][nc] && map[nr][nc] < size) fish.offer(new Node(nr, nc, n.cnt + 1));
 				
 				visited[nr][nc] = true;
-				queue.offer(new Node(nr, nc, n.cnt + 1));
+				fish.offer(new Node(nr, nc, n.cnt + 1));
 			}
 		}
+		return null;
 	}
 }
