24e5a2db91d4d1aad2669f2ac4044f1d7c37a581
김혜인 <fallingstars2749@gmail.com>
2022-05-03 22:40:50 +0900

    220503 프로그래머스 부족한금액계산하기

---
+++ Programmers/src/부족한금액계산하기.java
@@ -0,0 +1,12 @@
+class 부족한금액계산하기 {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+
+        long sum = 0;
+        for(int i = 1; i <= count; i++) {
+            sum += price * i;
+        }
+
+        return (sum > money) ? (sum - money) : 0;
+    }
+}
