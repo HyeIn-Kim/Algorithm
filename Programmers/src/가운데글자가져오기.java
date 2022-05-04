e875c1e3ce7e2682382db838fd531cf2c889f971
김혜인 <fallingstars2749@gmail.com>
2022-05-04 21:33:57 +0900

    220504 프로그래머스 가운데글자가져오기

---
+++ Programmers/src/가운데글자가져오기.java
@@ -0,0 +1,14 @@
+class 가운데글자가져오기 {
+    public String solution(String s) {
+        String answer = "";
+        if(s.length() % 2 == 0) {
+            int start = s.length() / 2 - 1;
+            answer = s.substring(start, start + 2);
+        }
+        else {
+            int start = s.length() / 2;
+            answer = s.substring(start, start + 1);
+        }
+        return answer;
+    }
+}
