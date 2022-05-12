7c4e64e76b223f3eb039c5a8ed1e876f5475f13e
김혜인 <fallingstars2749@gmail.com>
2022-05-12 21:01:52 +0900

    220512 프로그래머스 두정수사이의합

---
+++ Programmers/src/두정수사이의합.java
@@ -0,0 +1,18 @@
+class 두정수사이의합 {
+    public long solution(int a, int b) {
+        if(a == b) return a;
+
+        long answer = 0;
+        if(a < b) {
+            for(int i = a; i <= b; i++) {
+                answer += i;
+            }
+        }
+        else {
+            for(int i = b; i <= a; i++) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
