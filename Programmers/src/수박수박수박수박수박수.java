c5579ace6f709a2bf11e3553da0c14c5375fd6ef
김혜인 <fallingstars2749@gmail.com>
2022-05-19 23:16:55 +0900

    220519 프로그래머스 수박수박수박수박수박수

---
+++ Programmers/src/수박수박수박수박수박수.java
@@ -0,0 +1,11 @@
+class 수박수박수박수박수박수 {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n/2; i++) {
+            sb.append("수박");
+        }
+
+        if(n % 2 != 0) sb.append("수");
+        return sb.toString();
+    }
+}
