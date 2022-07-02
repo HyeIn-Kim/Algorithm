e2f8fdf28b1f5c592b4e4ea04c3787f2c4ddcf0a
김혜인 <fallingstars2749@gmail.com>
2022-07-02 23:59:27 +0900

    220702 백준 삼각형만들기2622

---
+++ Baekjoon/src/삼각형만들기2622.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 삼각형만들기2622 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int a = 1; a < N; a++) {
+            for(int b = a; b < N; b++) {
+                int c = N - a - b;
+
+
+            }
+        }
+    }
+}
