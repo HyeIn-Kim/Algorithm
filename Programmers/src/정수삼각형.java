62bd56a936a2e79de5393055e4e5c9592293ce4b
김혜인 <fallingstars2749@gmail.com>
2022-07-09 16:28:44 +0900

    220709 프로그래머스 정수삼각형(개선안)

--- Programmers/src/정수삼각형.java
+++ Programmers/src/정수삼각형.java
@@ -1,18 +1,16 @@
 class 정수삼각형 {
     public int solution(int[][] triangle) {
-        int length = triangle.length;
-        int[][] DP = new int[length][length];
-        DP[0][0] = triangle[0][0];
-        for(int i = 1; i < length; i++) {
-            for(int j = 0; j <= i; j++) {
-                if(j - 1 < 0) DP[i][j] = DP[i-1][j] + triangle[i][j];
-                else DP[i][j] = Math.max(DP[i-1][j-1], DP[i-1][j]) + triangle[i][j];
+        for(int i = 1; i < triangle.length; i++) {
+            triangle[i][0] = triangle[i-1][0];
+            triangle[i][i] = triangle[i-1][i-1];
+            for(int j = 1; j < i; j++) {
+                triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
             }
         }
 
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i < length; i++) {
-            max = Math.max(DP[length-1][i], max);
+        for(int i = 0; i < triangle.length; i++) {
+            max = Math.max(triangle[triangle.length-1][i], max);
         }
 
         return max;
