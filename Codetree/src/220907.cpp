7ae580de0debb10b8a62a70fd7dd68083ebbfe89
김혜인 <fallingstars2749@gmail.com>
2022-09-07 07:45:38 +0900

    220907 Codetree C++ 연습

---
+++ Codetree/src/220907.cpp
@@ -0,0 +1,87 @@
+// 변수 사용
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 97;
+    int b = 13;
+
+    cout << a << " - " << b << " = " << a - b;
+    return 0;
+}
+
+// 다양한 자료형의 변수 선언하기
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    char b = 'C';
+
+    cout << a << endl;
+    cout << b;
+    return 0;
+}
+
+// 변수로 곱셈하기
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 26;
+    int b = 5;
+
+    cout << a << " * " << b << " = " << a * b;
+    return 0;
+}
+
+// 세 정수형 변수 선언
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7;
+    int b = 23;
+    int c = 30;
+
+    cout << a << " + " << b << " = " << c;
+    return 0;
+}
+
+// string 자료형을 사용할때는 반드시 string을 include
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 3;
+    string b = "C";
+    cout << a << "..." << b;
+    return 0;
+}
+
+// 변수 출력 2
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    char b = 'C';
+    cout << b << "!.....!" << a;
+    return 0;
+}
+
+// 변수 출력 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+
+    cout << a << "->" << b << "->" << c;
+    return 0;
+}
+
+
