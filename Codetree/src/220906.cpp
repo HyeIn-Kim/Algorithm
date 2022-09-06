0d4a298dc5abccc14e94a53b30601608944d136c
김혜인 <fallingstars2749@gmail.com>
2022-09-06 22:53:41 +0900

    220906 Codetree C++연습

---
+++ Codetree/src/220906.cpp
@@ -0,0 +1,95 @@
+// 기본 틀
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+
+// 단어 출력
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout은 <<, cin은 >> 방향
+    cout << "Hello";
+    return 0;
+}
+
+// 문장 출력
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
+
+// 특수문자 출력은 \ + 특수문자
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "He says \"It's a really simple sentence\".";
+    return 0;
+}
+
+// 줄바꿈은 endl 또는 "\n"
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout << "Hello\nWorld";
+    cout << "Hello" << endl << "World";
+    return 0;
+}
+
+// 숫자는 그냥 숫자 3 또는 문자열 "3" 동일
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 3;
+    return 0;
+}
+
+// 사이에 공백을 넣을 때는 이렇게 
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout << "3 5";
+    cout << 3 << " " << 5;
+    return 0;
+}
+
+// 한줄 출력
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Let's go LeebrosCode!";
+    return 0;
+}
+
+// 두줄 출력
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello students!" << endl;
+    cout << "Welcome to LeebrosCode!";
+    return 0;
+}
+
+// 다양하게 출력
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Total days in Year" << endl;
+    cout << 365 << endl;
+    cout << "Circumference rate" << endl;
+    cout << "3.1415926535";
+    return 0;
+}
