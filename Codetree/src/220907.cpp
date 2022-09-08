1ae32405f213c4440c3efb8a096189c73acad285
김혜인 <fallingstars2749@gmail.com>
2022-09-08 22:17:49 +0900

    220908 C++ 연습

--- Codetree/src/220907.cpp
+++ Codetree/src/220907.cpp
@@ -112,3 +112,18 @@ int main() {
     cout << a;
     return 0;
 }
+
+// 단위 변환
+#include <iostream>
+using namespace std;
+
+int main() {
+    double ft = 30.48;
+    double mi = 160934;
+
+    cout << fixed;
+    cout.precision(1);
+    cout << 9.2 << "ft = " << 9.2 * ft << "cm" << endl;
+    cout << 1.3 << "mi = " << 1.3 * mi << "cm";
+    return 0;
+}
