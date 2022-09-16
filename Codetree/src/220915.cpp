0ae58b076fbc41e7e4d8031dd7b29d73eb0548b6
김혜인 <fallingstars2749@gmail.com>
2022-09-16 23:25:21 +0900

    220916 Codetree C++

--- Codetree/src/220915.cpp
+++ Codetree/src/220915.cpp
@@ -109,3 +109,66 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for(int testCase = 1; testCase <= T; testCase++) {
+        int k;
+        cin >> k;
+
+        set<int> s;
+        for(int i = 0; i < k; i++) {
+            char cmd;
+            int n;
+            cin >> cmd >> n;
+
+            switch(cmd) {
+                case 'I': s.insert(n); break;
+                case 'D':
+                    if(s.empty()) break;
+                    if(n == 1) s.erase(*s.rbegin());
+                    else s.erase(*s.begin());
+                    break;
+            }
+        }
+        
+        if(s.empty()) cout << "EMPTY" << endl;
+        else cout << *s.rbegin() << " " << *s.begin() << endl;
+    }
+    
+    return 0;
+}
+
+#include <iostream>
+#include <set>
+#include <utility>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    set<pair<int, int> > s;
+    for(int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        s.insert(make_pair(x, y));
+    }
+
+    for(int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        // pair의 첫번째 값의 lower bound, 값이 같다면 두번째 값의 lower bound를 반환
+        pair<int, int> result = *s.lower_bound(make_pair(x, y));
+
+        if(*s.end() == result) cout << "-1 -1" << endl;
+        else cout << result.first << " " << result.second << endl;
+    }
+    return 0;
+}
