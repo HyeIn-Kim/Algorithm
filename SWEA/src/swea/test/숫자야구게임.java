29a97686073635eb9df196ae50913132c0195c7c
김혜인 <fallingstars2749@gmail.com>
2022-09-20 00:04:10 +0900

    220920 SWEA 숫자야구게임

--- SWEA/src/swea/test/숫자야구게임.java
+++ SWEA/src/swea/test/숫자야구게임.java
@@ -1,162 +1,81 @@
-package swea.test;
-
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public class 숫자야구게임 {
+    class Fail {
+        int[] guess;
+        int strike, ball;
+
+        public Fail(int[] guess, int strike, int ball) {
+            this.guess = guess;
+            this.strike = strike;
+            this.ball = ball;
+        }
+    }
+
     public final static int N = 4;
 
-    Solution.Result prevRes;
-    int[] prev;
-
     boolean[] selected;
-    boolean[] notAnswer;
-    int[] strikes;
+    int[] guess;
+    ArrayList<Fail> failDatas;
 
-    boolean isFirst;
     boolean isAnswer;
 
-    HashSet<String> queries;
-
-    public void doUserImplementation(int guess[]) {
+    public void doUserImplementation(int answer[]) {
         selected = new boolean[10];
-        notAnswer = new boolean[10];
-        strikes = new int[4];
-        for(int i = 0; i < 4; i++) {
-            strikes[i] = -1;
-        }
+        guess = new int[4];
+        failDatas = new ArrayList<>();
 
-        prevRes = null;
-        prev = new int[4];
-
-        isFirst = true;
         isAnswer = false;
-        queries = new HashSet<>();
-        permutation(0, new int[4], guess);
+        permutation(0, answer);
     }
 
-    private void saveResult(Solution.Result res, int[] guess) {
-        prevRes = res;
-        for(int i = 0; i < 4; i++) {
-            prev[i] = guess[i];
-        }
-    }
-
-    private void permutation(int cnt, int guess[], int[] answer) {
+    private void permutation(int cnt, int[] answer) {
         if(isAnswer) return;
         if(cnt == 4) {
-            if(queries.contains(Arrays.toString(guess))) return;
+            if(check()) {
+                Solution.Result res = Solution.query(guess);
 
-            Solution.Result res = Solution.query(guess);
-            queries.add(Arrays.toString(guess));
-//            System.out.println(Arrays.toString(notAnswer));
-//            System.out.println(Arrays.toString(strikes));
-//            System.out.println(Arrays.toString(guess) + " " + res.strike + " " + res.ball);
-//            System.out.println(res.strike + " " + res.ball);
-
-            // 정답을 찾았을 경우 종료
-            if(res.strike == 4) {
-                for(int i = 0; i < 4; i++) {
-                    answer[i] = guess[i];
-                }
-                isAnswer = true;
-                return;
-            }
-
-            // 스트라이크, 볼이 없는 경우: 정답이 아니므로 제외한다.
-            if(res.strike == 0 && res.ball == 0) {
-                for(int i = 0; i < 4; i++) {
-                    notAnswer[guess[i]] = true;
-                }
-            }
-
-            if(isFirst) {
-                isFirst = false;
-                // 현재 숫자와 결과를 저장
-                saveResult(res, guess);
-                return;
-            }
-
-            // 스트라이크 증가: 현재 숫자 스트라이크 기록
-            if(prevRes.strike < res.strike) {
-                int strikeCnt = 0;
-                int idx = 0;
-                for(int i = 0; i < 4; i++) {
-                    if(prev[i] != guess[i]) {
-                        strikeCnt++;
-                        idx = i;
-                    }
-                }
-
-                if(strikeCnt == 1) {
-                    strikes[idx] = guess[idx];
-                }
-                else return;
-
-                // 볼 개수가 같은 경우, 이전 숫자는 정답이 아님
-                if(prevRes.ball == res.ball) {
+                if(res.strike == 4) {
+                    isAnswer = true;
                     for(int i = 0; i < 4; i++) {
-                        if(prev[i] != guess[i]) {
-                            notAnswer[prev[i]] = true;
-                            break;
-                        }
+                        answer[i] = guess[i];
                     }
+
+                    return;
                 }
 
-                // 볼 개수가 줄어든 경우, 이전 숫자는 볼
-            }
-
-            // 스트라이크 증가: 이전 숫자 스트라이크 기록
-            if(prevRes.strike > res.strike) {
-                int strikeCnt = 0;
-                int idx = 0;
+                int[] resNum = new int[4];
                 for(int i = 0; i < 4; i++) {
-                    if(prev[i] != guess[i]) {
-                        strikeCnt++;
-                        idx = i;
-                    }
+                    resNum[i] = guess[i];
                 }
 
-                if(strikeCnt == 1) {
-                    strikes[idx] = prev[idx];
-                }
-                else return;
-
-                // 볼 개수가 같은 경우, 이전 숫자는 정답이 아님
-                if(prevRes.ball == res.ball) {
-                    for(int i = 0; i < 4; i++) {
-                        if(prev[i] != guess[i]) {
-                            notAnswer[prev[i]] = true;
-                            break;
-                        }
-                    }
-                }
-
-                // 볼 개수가 늘어난 경우, 현재 숫자는 볼
+                failDatas.add(new Fail(resNum, res.strike, res.ball));
             }
 
-            // 현재 숫자와 결과를 저장
-            saveResult(res, guess);
             return;
         }
 
         for(int i = 0; i < 10; i++) {
             if(selected[i]) continue;
-            if(notAnswer[i]) continue;
 
-            if(strikes[cnt] > -1) {
-                selected[strikes[cnt]] = true;
-                guess[cnt] = strikes[cnt];
-                permutation(cnt + 1, guess, answer);
+            selected[i] = true;
+            guess[cnt] = i;
+            permutation(cnt + 1, answer);
+            selected[i] = false;
+        }
+    }
+
+    private boolean check() {
+        for(Fail failData : failDatas) {
+            int strike = 0, ball = 0;
+            for(int i = 0; i < 4; i++) {
+                if(guess[i] == failData.guess[i]) strike++;
+                else if(selected[failData.guess[i]]) ball++;
             }
-            else {
-                selected[i] = true;
-                guess[cnt] = i;
-                permutation(cnt + 1, guess, answer);
-                selected[i] = false;
-            }
+
+            if(strike != failData.strike || ball != failData.ball) return false;
         }
 
-        permutation(cnt + 1, guess, answer);
+        return true;
     }
 }
