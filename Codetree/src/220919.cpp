564b4a87e45b1524014a53823a3c7ef92d91ce64
김혜인 <fallingstars2749@gmail.com>
2022-10-03 23:36:33 +0900

    221003 Codetree C++

--- Codetree/src/220919.cpp
+++ Codetree/src/220919.cpp
@@ -92,3 +92,18 @@ int main() {
     else cout << 0;
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a % 3 == 0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+    if(a % 5 == 0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
