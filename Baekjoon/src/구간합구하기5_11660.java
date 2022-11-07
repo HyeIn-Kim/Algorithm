a1dad337de463c0bf745c669a0e0d37efab4bdc0
김혜인 <fallingstars2749@gmail.com>
2022-11-08 07:50:36 +0900

    221108 백준 구간합구하기5_11660

--- Baekjoon/src/구간합구하기5_11660.java
+++ Baekjoon/src/구간합구하기5_11660.java
@@ -3,9 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 1 2   1 3
+// 2 3   3 ?  => 3 + 3 - 1(중복됐으니 빼줌) + 3
+// 구간합 부분도 범위의 윗부분, 왼쪽 빼주면 겹치는 부분 2번 빼니까 다시 1번 더해줌!
 public class 구간합구하기5_11660 {
     static int N, M;
-    static int[][] numbers;
     static int[][] DP;
 
     public static void main(String[] args) throws IOException {
@@ -14,22 +16,13 @@ public class 구간합구하기5_11660 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        numbers = new int[N + 1][N + 1];
         DP = new int[N + 1][N + 1];
         for(int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            DP[i][0] = DP[i - 1][N];
             for(int j = 1; j <= N; j++) {
-                numbers[i][j] = Integer.parseInt(st.nextToken());
-                DP[i][j] = DP[i][j-1] + numbers[i][j];
+                int number = Integer.parseInt(st.nextToken());
+                DP[i][j] = DP[i-1][j] + DP[i][j-1] - DP[i-1][j-1] + number;
             }
-        }
-
-        for(int i = 0; i <= N; i++) {
-            for(int j = 0; j <= N; j++) {
-                System.out.print(DP[i][j] + " ");
-            }
-            System.out.println();
         }
 
         StringBuilder sb = new StringBuilder();
@@ -40,11 +33,7 @@ public class 구간합구하기5_11660 {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            if(x1 == x2 && y1 == y2) sb.append(numbers[x1][y1]);
-            else if(x2 - x1 == N-1 && y2 - y1 == N-1) sb.append(DP[x2][y2]);
-            else sb.append(DP[x2][y2] - DP[x1][y1]);
-
-            sb.append("\n");
+            sb.append(DP[x2][y2] - DP[x1 - 1][y2] - DP[x2][y1 - 1] + DP[x1 - 1][y1 - 1]).append("\n");
         }
 
         System.out.println(sb);
