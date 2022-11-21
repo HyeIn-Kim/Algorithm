e26ea00d6dbe8cf200af192fd413c9507da3de05
김혜인 <fallingstars2749@gmail.com>
2022-11-21 23:58:01 +0900

    221120 백준 친구비16562(푸는중)

---
+++ Baekjoon/src/친구비16562.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 친구비16562 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
